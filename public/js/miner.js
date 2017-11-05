6cd2d501e435f5af4d10a57d6418dbe4a4d53ee4
Elion <dz_elf@mail.ru>
2017-11-05 22:58:52 +0300

    PhaserJS first demo

--- public/js/miner.js
+++ public/js/miner.js
@@ -7,37 +7,43 @@ var miner = (function () {
     var game = new Phaser.Game(800, 600, Phaser.AUTO, 'phaser-example', { preload: preload, create: create, update: update, render: render });
     
     function preload() {
-        game.load.image('arrow', 'static/img/1.png');
+        game.time.advancedTiming = true;
+        game.load.image('arrow', 'static/img/2.png');
     }
-    
-    var sprite;
+    var sprites = [];
     
     function create() {
     
         game.physics.startSystem(Phaser.Physics.ARCADE);
     
-        game.stage.backgroundColor = '#0072bc';
+        game.stage.backgroundColor = '#343a40';
     
-        sprite = game.add.sprite(400, 300, 'arrow');
-        sprite.anchor.setTo(0.5, 0.5);
-    
-        //  Enable Arcade Physics for the sprite
-        game.physics.enable(sprite, Phaser.Physics.ARCADE);
-    
-        //  Tell it we don't want physics to manage the rotation
-        sprite.body.allowRotation = false;
+        for (var i = 0; i < 5; i++) {
+          var sprite = game.add.sprite(100 + i * 100, 300, 'arrow');
+          sprite.anchor.setTo(0.5, 0.5);
+      
+          //  Enable Arcade Physics for the sprite
+          game.physics.enable(sprite, Phaser.Physics.ARCADE);
+      
+          //  Tell it we don't want physics to manage the rotation
+          sprite.body.allowRotation = false;
+          sprites.push(sprite);
+        }
+
     
     }
     
     function update() {
-    
-        sprite.rotation = game.physics.arcade.moveToPointer(sprite, 60, game.input.activePointer, 500) + Math.PI / 2;
+        for (var sp in sprites) {
+          var sprite = sprites[sp];
+          sprite.rotation = game.physics.arcade.moveToPointer(sprite, 60, game.input.activePointer, 1000) + Math.PI / 2;
+        }
     
     }
     
     function render() {
-    
-        game.debug.spriteInfo(sprite, 32, 32);
+        game.debug.text(game.time.fps || '--', 2, 14, "#00ff00");   
+        // game.debug.spriteInfo(sprite, 32, 32);
     
     }
     
